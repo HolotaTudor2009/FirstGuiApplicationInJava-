cb77c4d8526ba8e054190936acee8a3748c01673
HolotaTudor2009 <78162323+HolotaTudor2009@users.noreply.github.com>
2021-04-05 12:30:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+public class Main {
+		public static void main(String args[]) {
+			/*
+			JFrame frame = new JFrame();
+			frame.setTitle("JFrame title goes here");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(false);
+			frame.setSize(420,420);
+			frame.setVisible(true);
+			ImageIcon image = new ImageIcon("unnamed.jpg");
+			frame.setIconImage(image.getImage());
+			frame.getContentPane().setBackground(new Color(123,50,250));
+			*/
+			MyFrame myFrame = new MyFrame();
+			}
+}
